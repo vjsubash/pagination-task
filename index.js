3c13e0487843c5eef0522e73e5b30ce41c72100e
vjsubash <vjsubash79@gmail.com>
2023-05-18 00:15:57 -0700

    pagination-task

---
+++ index.js
@@ -0,0 +1,31 @@
+// let link = document.getElementsByClassName("link");
+
+// let currentValue = 1;
+
+// function activeLink(){
+//     for(l of link){
+//         l.classList.remove("active");
+//     }
+//     Event.target.classList.add("active");
+//     currentValue = Event.target.value;
+// }
+
+// function backBtn(){
+//     if(currentValue > 1){
+//         for(1 of link){
+//             l.classList.remove("active");
+//         }
+//         currentValue--;
+//         link[currentValue-1].classList.add("active");
+//     }
+// }
+
+// function nextBtn(){
+//     if(currentValue > 6){
+//         for(1 of link){
+//             l.classList.remove("active");
+//         }
+//         currentValue++;
+//         link[currentValue-1].classList.add("active");
+//     }
+// }
